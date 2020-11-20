118749ab1b57202b0fac1f22a3e1dd4bd4769b40
bod <zagorboda@gmail.com>
2020-11-20 23:08:05 +0200

    Add comments to post detail

--- api/serializers.py
+++ api/serializers.py
@@ -1,24 +1,45 @@
 from rest_framework import serializers
 
-from blog_app.models import Post
+from blog_app.models import Post, Comment
 
 # from user_app.models import CustomUser
 from django.contrib.auth.models import User
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    # author = serializers.ReadOnlyField(source='author.username')
+    author = serializers.HyperlinkedRelatedField(view_name='user-detail', read_only=True, lookup_field='username')
+
+    class Meta:
+        model = Comment
+        fields = ('author', 'body', 'created_on')
 
 
 class PostSerializer(serializers.HyperlinkedModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name='post-detail', lookup_field='slug')
     owner = serializers.ReadOnlyField(source='author.username')
     # owner = serializers.HyperlinkedRelatedField(view_name='user-detail', read_only=True)
+    # comments = serializers.HyperlinkedRelatedField(many=True, view_name='', read_only=True, lookup_field='slug')
+    # comments = serializers.PrimaryKeyRelatedField(many=True, queryset=Comment.objects.all())
+
+    comments = CommentSerializer(many=True)
 
     class Meta:
         model = Post
-        fields = ['url', 'id', 'title', 'content', 'slug', 'owner', 'created_on']
+        fields = ['url', 'id', 'title', 'content', 'slug', 'owner', 'created_on', 'comments']
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name='user-detail', lookup_field='username')
     posts = serializers.HyperlinkedRelatedField(many=True, view_name='post-detail', read_only=True, lookup_field='slug')
+    # posts = serializers.SerializerMethodField('get_published_posts')
+
+    # TODO : if other user make request, return only published posts, if owner make request return
+    #  all posts, marked as draft/posted(return status)
+    # def get_published_posts(self, user):
+    #     posts = Post.objects.filter(status=1)
+    #     serializer = PostSerializer(instance=posts, many=True)
+    #     return serializer.data
 
     class Meta:
         model = User
