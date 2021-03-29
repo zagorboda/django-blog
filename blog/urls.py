bca21fa6d687adcc2824310e2f401c66307be559
bod <zagorboda@gmail.com>
2021-03-29 13:08:44 +0300

    Fix search and tests

--- blog/urls.py
+++ blog/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
 
@@ -23,5 +22,4 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('user/', include('user_app.urls')),
-    path('__debug__/', include(debug_toolbar.urls)),
 ]
